06753c69f6c01cdc1e0f0d4f1bb06dcd5d987a02
James <chongogs@live.com>
2014-05-25 19:55:17 +1000

    Car Code

---
+++ Car.java
@@ -0,0 +1,76 @@
+/**
+ * 
+ /**
+ * 
+ * This file is part of the CarParkSimulator Project, written as 
+ * part of the assessment for INB370, semester 1, 2014. 
+ *
+ * CarParkSimulator
+ * asgn2Vehicles 
+ * 20/04/2014
+ * 
+ */
+package asgn2Vehicles;
+
+import asgn2Exceptions.VehicleException;
+import asgn2Vehicles.Vehicle;
+
+/**
+ * The Car class is a specialisation of the Vehicle class to cater for production cars
+ * This version of the class does not cater for model types, but records whether or not the 
+ * vehicle can use a small parking space. 
+ * 
+ * @author hogan
+ *
+ */
+public class Car extends Vehicle {
+ 
+ private boolean small;
+ private int arrivalTime;
+ private String vehID;
+ public Car c;
+
+ /**
+  * The Car Constructor - small set at creation, not mutable. 
+  * @param vehID - identification number or plate of the vehicle
+  * @param arrivalTime - time (minutes) at which the vehicle arrives and is 
+  *        either queued or given entry to the carpark 
+  * @param small - indicator whether car is regarded as small or not
+  * @throws VehicleException if arrivalTime is <= 0  
+  */
+ public Car(String vehID, int arrivalTime, boolean small) throws VehicleException {
+  
+  super(vehID, arrivalTime);
+  
+  if (arrivalTime <=0) {
+     throw new VehicleException("The arrival time is wrong!");   
+  }
+  else {
+     this.small = small;
+     this.arrivalTime = arrivalTime;
+     this.vehID = vehID;     
+  }
+ }
+
+ /**
+  * Boolean status indicating whether car is small enough for small 
+  * car parking spaces  
+  * @return true if small parking space, false otherwise
+  */
+ public boolean isSmall() {
+  if (small == true){
+   return true;
+  }
+  else {
+   return false;
+  }
+ }
+
+ /* (non-Javadoc)
+  * @see asgn2Vehicles.Vehicle#toString()
+  */
+ @Override
+ public String toString() {
+  return vehID + " " + arrivalTime + " " + small;
+ }
+}
